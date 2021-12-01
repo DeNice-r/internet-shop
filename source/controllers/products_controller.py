ad950abe752536a610587d8dfa597a35310ae920
DeNice-r <52076437+DeNice-r@users.noreply.github.com>
2021-12-01 23:31:07 +0200

    Products pagination and mdb5 first touch.

--- source/controllers/products_controller.py
+++ source/controllers/products_controller.py
@@ -1,7 +1,7 @@
 import json
 
 from app import app, db
-from flask import render_template
+from flask import render_template, abort
 from models.product import Product
 from math import ceil
 
@@ -12,25 +12,18 @@ def products(page: int):
     items_per_page = 8
     page -= 1
 
-    # p = Product("Амогус портфель молодіжний", "['amogus-portfel.jpg']", 333.33, 33.03, 4,
-    #             "{"
-    #             "'крутість': 'крутий шо капєц',"
-    #             "'об\'єм': 'ну думаю літри 3-4 буде',"
-    #             "'розміри': 'та на дитя налізе навєрно'"
-    #             "}", "Ну класний молодіжний портфель. Було б мені 0 років, купив би собі такий теж щоб пофлєксить "
-    #                  "перед однокласниками.")
-    # db.session.add(p)
-    # db.session.commit()
+    products = Product.query.order_by(Product.id.desc()).limit(items_per_page).offset(page * items_per_page).all()
+
+    if not products:
+        abort(404)
+
     return render_template("products/products.html",
-                           products=Product.query.limit(items_per_page).offset(page * items_per_page).all(),
-                           number_of_pages=ceil(Product.query.count() / items_per_page)
+                           products=products,
+                           number_of_pages=ceil(Product.query.count() / items_per_page),
+                           page=page + 1
                            )
 
 
 @app.route("/product/<int:id>")
 def product(id: int):
-    ps = Product.query.all()
-    for p in ps:
-        p.pictures = ['amogus-portfel.jpg']
-    db.session.commit()
     return render_template("products/product.html", product=Product.query.get(id), json=json)
